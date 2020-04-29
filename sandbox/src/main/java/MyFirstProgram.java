15d604524c3b16541866d1f57feefd55a622f67d
alexrecoon <52834857+alexrecoon@users.noreply.github.com>
2020-04-29 19:11:20 +0300

    Создал проект в idea, прикрутил модули build в src

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,9 +1,9 @@
 public class MyFirstProgram {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-System.out.println("Hello, world");
+        System.out.println("Hello, world");
 
-}
+    }
 
 }
